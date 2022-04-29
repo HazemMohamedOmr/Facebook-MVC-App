89f6489943df9deea3255394afe687604cd11db8
HazemMohamedOmr <55815459+HazemMohamedOmr@users.noreply.github.com>
2022-04-29 23:58:23 +0200

    First Migration

--- Facebook/Program.cs
+++ Facebook/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<ApplicationDBContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("ConStr1")));
+builder.Services.AddDbContext<ApplicationDBContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("ConStr")));
 
 var app = builder.Build();
 
